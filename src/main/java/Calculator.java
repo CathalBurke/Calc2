92a013ac6883666903c179885c40eca24fdc7bf1
Cathal <g00413483@atu.ie>
2025-03-24 20:17:35 +0000

    Added multiply functionality and ran tests and got 100% coverage

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -16,4 +16,11 @@ public class Calculator {
         }
         return (int) result;
     }
+    public int multiply(int firstNum, int secondNum) {
+        long result = (long) firstNum * secondNum;
+        if(result < Integer.MIN_VALUE || result > Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("Your values are too large");
+        }
+        return (int) result;
+    }
 }
